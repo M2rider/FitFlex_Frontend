1f76f19fb619b482f4862f4100640b44fccbb6fd
M2rider <mayank.singhal1703@gmail.com>
2024-04-13 09:29:07 +0530

    f1

--- src/Auth/user/UserSignup.jsx
+++ src/Auth/user/UserSignup.jsx
@@ -18,7 +18,7 @@ export default function UserSignup() {
         e.preventDefault();
         try {
             const res = await axios.post(
-                `https://vihaan-fitflex.onrender/api/v1/auth/register`,
+                `https://vihaan-fitflex.onrender.com/api/v1/auth/register`,
                 {name, email, password,address}
             );
             if(res && res.data.success){
